43d7eb1453d76f4ce03fe8f41d1cfc67d58ae3af
vinraelll <53097647+vinraelll@users.noreply.github.com>
2020-06-01 18:11:14 +0300

    Добавил в репозиторий папки с проектом MartaUP

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 2000,
+
+    });
+
+});
